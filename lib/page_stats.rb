864750e5fb22ec8882b39432fbb93709c707b12b
Vlad Mehakovic <vladiim@yahoo.com.au>
2015-02-20 17:34:16 +1100

    Removed refactored code

--- lib/page_stats.rb
+++ lib/page_stats.rb
@@ -1,12 +1,8 @@
 class PageStats
-  API = 'https://api.engine.priceonomics.com/v1/apps/'
 
-  attr_reader :url, :access_key, :headers, :values
+  attr_reader :url
   def initialize(url)
-    @url        = url
-    @access_key = ENV['PRICE_ACCESS_KEY']
-    @headers    = { x_access_key: access_key }
-    @values     = { 'async' => false, 'data' => { 'url' => url } }.to_json
+    @url = url
   end
 
   def data
@@ -15,96 +11,13 @@ class PageStats
 
   def social_data
     @social_data ||= SocialDataGetter.new(url).data
-    # @social_data ||= social_page_stats(attempt_get_social_data)
   end
 
   def reading_level_data
     @reading_level_data ||= ReadingLevelDataGetter.new(url).data
-    # @reading_level_data ||= reading_level_page_stats(attempt_get_reading_data)
   end
 
   def content_data
     @content_data ||= ContentDataGetter.new(url).data
-    # @content_data ||= content_page_data(attempt_get_content_data)
   end
-
-  # def article_data
-  #   @article_data ||= 
-  # end
-
-  private
-
-  # def social_page_stats(data)
-  #   return data if data == request_timeout_error_data
-  #   SocialPageStats.new(data).data
-  # end
-
-  # def reading_level_page_stats(data)
-  #   return data if data == request_timeout_error_data
-  #   ReadingLevelPageStats.new(data).data
-  # end
-
-  # def content_page_data(data)
-  #   return data if data == request_timeout_error_data
-  #   ContentPage.new(data).data
-  # end
-
-  # def attempt_get_social_data
-  #   begin
-  #     get_social_data
-  #   rescue RestClient::RequestTimeout
-  #     request_timeout_error_data
-  #   end
-  # end
-
-  # def attempt_get_reading_data
-  #   begin
-  #     get_reading_data
-  #   rescue RestClient::RequestTimeout
-  #     request_timeout_error_data
-  #   end
-  # end
-
-  # def attempt_get_content_data
-  #   begin
-  #     get_content_data
-  #   rescue RestClient::RequestTimeout
-  #     request_timeout_error_data
-  #   end
-  # end
-
-  # def get_social_data
-  #   RestClient.post("#{ API }social", values, headers)
-  # end
-
-  # def get_reading_data
-  #   RestClient.post("#{ API }readinglevel", reading_values, headers)
-  # end
-
-  # def get_content_data
-  #   RestClient.post("#{ API }fetch", content_values, headers)
-  # end
-
-  # def reading_values
-  #   {
-  #       async: false,
-  #       data: {
-  #           content: 'content',
-  #       }
-  #   }.to_json
-  # end
-
-  # def content_values
-  #   {
-  #     async: false,
-  #     data: {
-  #       url: url,
-  #       obey_robots: false
-  #     }
-  #   }.to_json
-  # end
-
-  # def request_timeout_error_data
-  #   { 'data' => { 'stats' => { error: 'request timeout' } } }.to_json
-  # end
 end
